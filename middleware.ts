9531a40f0e8afc81806dfb1dba727516a357a030
Abdiel Otzoy <abdielotzoy@gmail.com>
2024-05-26 21:26:53 -0600

    feat: add ignored routes for Clerk and Stripe webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks/clerk"],
+  ignoredRoutes: ["/","/api/webhooks/clerk","/api/webhooks/stripe"],
 });
 
 export const config = {
